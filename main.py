a50ff4d280742287073ea30fd27e31cf9288541c
Kotta <mathieu2008blanche@gmail.com>
2025-03-07 19:48:38 +0100

    refactor: change assets fp refactor: delete image param in methods functions feat: add the player-icon to the image feat: organize elements

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
 import requests
-from PIL import Image, ImageDraw
 from methods import profile, error
 
 API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE5OGNkNDljLWJjOTAtNGM5YS04ZWM5LTcxZjQ5MmVhZjgwZiIsImlhdCI6MTc0MTAyNDEyOSwic3ViIjoiZGV2ZWxvcGVyLzkxY2JiZDI3LWE1MDktZmNjZC01OGM1LTYyODI0MzRjMTFhZiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTc2LjE5MS4yMDEuODkiXSwidHlwZSI6ImNsaWVudCJ9XX0.pfsB4jwooLcnb2wwjhaRS_NqAx-CKgbJRhZLQMEGC_kjtA9yF86lWEFidUD80QPBKmVspCedxB6jApsJczmDUg"
@@ -10,15 +8,12 @@ headers = {
     "Accept": "application/json"
 }
 
-image = Image.open("assets/brawl_stars_lobby.jpg")
-
 tag = str(input("Enter your tag : #"))
 response = requests.api.get(f"https://api.brawlstars.com/v1/players/%23{tag}", headers=headers)
 if response.status_code == 200:
     data = response.json()
-    image = profile(image, data)
+    image = profile(data)
     image.show()
 else:
-    image = error(image, response)
+    image = error(response)
     image.show()
-
--- methods.py
+++ methods.py
@@ -1,14 +1,28 @@
 from PIL import Image, ImageDraw
+from io import BytesIO
+import requests
 
-def profile(image, data):
+image = Image.open("assets/img/brawl_stars_lobby.jpg")
+
+def profile(data):
+
+    icon_id = data['icon']['id']
+
+    icon_request = requests.get(f"https://cdn.brawlify.com/profile-icons/regular/{icon_id}.png")
+    if icon_request.status_code == 200:
+        icon = Image.open(BytesIO(icon_request.content))
+
+    name = data['name']
+    name_color = data['nameColor']
+
+    image.paste(icon, (5, 5))
 
     imageDraw = ImageDraw.Draw(image)
-
-    imageDraw.text((10, 10), data['name'], font_size=100)
+    imageDraw.text((215, 0), name, font_size=100, stroke_fill=name_color)
 
     return image
 
-def error(image, response):
+def error(response):
     imageDraw = ImageDraw.Draw(image)
 
     imageDraw.text((10, 10), f"Error: {str(response.status_code)}", font_size=100)
