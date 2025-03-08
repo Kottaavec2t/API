33a0727e4b93e555e3588905ab1a73f03cf56f52
Kotta <mathieu2008blanche@gmail.com>
2025-03-08 21:17:00 +0100

    feat: add 3v3 and brawlers count display

--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 from io import BytesIO
 import requests
+import API_KEY
 
 image = Image.open("assets/img/brawl_stars_lobby.jpg")
 imageDraw = ImageDraw.Draw(image)
@@ -10,6 +11,8 @@ display_max_trophies = Image.open("assets/img/display_max_trophies_bar.png")
 display_level = Image.open("assets/img/display_level_bar.png")
 display_solo_win = Image.open("assets/img/display_solo_win_bar.png")
 display_duo_win = Image.open("assets/img/display_duo_win_bar.png")
+display_trio_win = Image.open("assets/img/display_trio_win_bar.png")
+display_brawlers = Image.open("assets/img/display_brawlers_count_bar.png")
 
 def _change_font(size):
     font = ImageFont.truetype("assets/fonts/lilitaone-regular-webfont (2).ttf", size)
@@ -31,11 +34,20 @@ def profile(data):
     level = data['expLevel']
     solo_win = data['soloVictories']
     duo_win = data['duoVictories']
-    treevstree_win = data['3vs3Victories']
+    trio_win = data['3vs3Victories']
     best_robot_rumble_time = data['bestRoboRumbleTime']
     best_big_brawler_time = data['bestTimeAsBigBrawler']
     championship_challenge_win = data['isQualifiedFromChampionshipChallenge']
     brawlers = data['brawlers']
+
+    #All Brawlers Request
+    headers = {
+        "Authorization": f"Bearer {API_KEY.API_KEY}",
+        "Accept": "application/json"
+    }
+    all_brawlers_request = requests.api.get("https://api.brawlstars.com/v1/brawlers", headers=headers)
+    all_brawlers_data = all_brawlers_request.json()
+    all_brawlers = all_brawlers_data['items']
 
     #Icon
     icon_request = requests.get(f"https://cdn.brawlify.com/profile-icons/regular/{icon_id}.png")
@@ -65,7 +77,21 @@ def profile(data):
     #Duo Win
     image.paste(display_duo_win, (725, -230), mask=display_duo_win)
     imageDraw.text((1060, 130), str(duo_win), fill="black", font=_change_font(50))
+
+    #Trio Win
+    image.paste(display_trio_win, (1125, -335), mask=display_trio_win)
+    imageDraw.text((1460, 25), str(trio_win), fill="black", font=_change_font(50))
     
+    #Brawlers
+    total_brawler_count = 0
+    brawler_count = 0
+    for brawler in all_brawlers:
+        total_brawler_count += 1
+    for brawler in brawlers:
+        brawler_count += 1
+    image.paste(display_brawlers, (1125, -230), mask=display_brawlers)
+    imageDraw.text((1460, 130), f"{str(brawler_count)}/{str(total_brawler_count)}", fill="black", font=_change_font(50))
+
     return image
 
 def error(response):
